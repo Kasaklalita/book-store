2a1619b73f03d3e0d4b7c6ae621b8f29cc92dbf6
Veniamin Polienko <kasaklalita@mail.ru>
2022-02-03 19:31:17 +0300

    added render for the post_detail page

--- book_outlet/views.py
+++ book_outlet/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Book
+from django.http import Http404
 # Create your views here.
 
 def index(request):
@@ -9,7 +10,11 @@ def index(request):
     })
 
 def book_detail(request, id):
-    book = Book.objects.get(pk = id)
+    # try:
+    #     book = Book.objects.get(id = id)
+    # except:
+    #     raise Http404()
+    book = get_object_or_404(Book, pk = id)
     return render(request, "book_outlet/book_detail.html", {
         "title": book.title,
         "author": book.author,
